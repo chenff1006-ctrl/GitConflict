ec7965a20ecb12dc26f9fe6f8a524e8ea8e48377
chenff <ffchenff@chinatelecom.cn>
2025-12-09 14:47:24 +0800

    初始版本

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <VideoPlayer />
+  </div>
+</template>
+
+<script>
+import VideoPlayer from './components/VideoPlayer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+html,body{
+  width: 100%;
+  height: 100%;
+}
+*{
+  padding: 0;
+  margin: 0;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>111</div>
+</template>
+<script>
+import AMapLoader from "@amap/amap-jsapi-loader";
+// 设置安全密钥
+window._AMapSecurityConfig = {
+  securityJsCode: "3ad824e800428cd441722d33b0e6f656",
+};
+export default {
+  mounted() {
+  },
+  data() {
+    return {
+    
+    };
+  }
+};
+</script>
